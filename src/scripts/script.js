0f4569fc806bb2a2c1b1d3d5b860302b27930f9c
WesleyTavaresDev <WesleyTavaresDev@outlook.com>
2022-11-21 19:01:29 -0300

    increase function wrote

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,4 +3,12 @@ let second = document.getElementById('seconds');
 
 function increase() {
     
+    
+    if(second.innerText < 50) 
+        second.innerText = parseInt(second.innerText) + 10;
+
+    else if (second.innerText >= 50) {
+        second.innerText = 00;
+        minute.innerText = parseInt(minute.innerText) + 1;
+    }
 }
