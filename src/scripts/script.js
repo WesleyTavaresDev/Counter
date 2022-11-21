d2ab1ea4f2e7c5155ac1ce04b0be248652587dd7
WesleyTavaresDev <WesleyTavaresDev@outlook.com>
2022-11-21 19:17:46 -0300

    fix => increase.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,10 +4,10 @@ let second = document.getElementById('seconds');
 function increase() {
     
     
-    if(second.innerText < 50) 
+    if(second.innerText < 60) 
         second.innerText = parseInt(second.innerText) + 10;
 
-    else if (second.innerText >= 50) {
+    else if (second.innerText >= 60) {
         second.innerText = 00;
         minute.innerText = parseInt(minute.innerText) + 1;
     }
